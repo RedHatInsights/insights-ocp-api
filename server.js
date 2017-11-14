2634131d817397dc022a85784720f061bb6bd1a6
Jeremy Crafts <jcrafts@redhat.com>
2017-11-14 16:42:51 -0500

    fix some spacing, ES6 syntax

--- server.js
+++ server.js
@@ -12,40 +12,40 @@ app.set('port', config.port);
 
 // routes
 app.post('/report', bodyParser.json(), (req, res) => {
-    try{
+    try {
 		const reportRow = req.body;
 		reportRow.createdAt = Date.now();
 
 		const reportModel = sqldb().report;
 		reportModel.create(reportRow)
-		.then((result) => {
+		.then(result => {
         	return res.status(201).send(result);
-    	}).catch((err) => {
+    	}).catch(err => {
     		return res.status(412).send(err);
-    	})
-	}catch(err){
+    	});
+	} catch (err) {
 		res.status(412).send(err);
 	}
 });
 
 app.get('/report/:id', (req, res) => {
-	try{
+	try {
 		const reportModel = sqldb().report;
 		reportModel.findOne({
 			where: {
 					id: req.params.id
 				}
 			})
-	        .then(function reportFindAll(reports) {
-	        	if (reports == null){
+	        .then(reports => {
+	        	if (reports === null) {
 	        		reports = {};
 	        	}
 	        	res.status(200).send(reports);
 	        })
-	        .catch(function(err){
+	        .catch(err => {
 	        	res.status(412).send(err);
 	        });
-	}catch(err){
+	} catch(err) {
 		res.status(412).send(err);
 	}
 });
