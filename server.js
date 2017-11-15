1ec714d055564a3f66ef90a23c1e928cab433b05
Richard Brantley <brantleyr@gmail.com>
2017-11-15 12:46:09 -0500

    do db sync on start

--- server.js
+++ server.js
@@ -13,50 +13,57 @@ app.set('port', config.port);
 // routes
 app.post('/report', bodyParser.json(), (req, res) => {
     try {
-		const reportRow = req.body;
-		reportRow.createdAt = Date.now();
+        const reportRow = req.body;
+        reportRow.createdAt = Date.now();
 
-		const reportModel = sqldb().report;
-		reportModel.create(reportRow)
-		.then(result => {
-        	return res.status(201).send(result);
-    	}).catch(err => {
-    		return res.status(412).send(err);
-    	});
-	} catch (err) {
-		res.status(412).send(err);
-	}
+        const reportModel = sqldb().report;
+        reportModel.create(reportRow)
+        .then(result => {
+            return res.status(201).send(result);
+        }).catch(err => {
+            return res.status(412).send(err);
+        });
+    } catch (err) {
+        res.status(412).send(err);
+    }
 });
 
 app.get('/report/:id', (req, res) => {
-	try {
-		const reportModel = sqldb().report;
-		reportModel.findOne({
-			where: {
-					id: req.params.id
-				}
-			})
-	        .then(reports => {
-	        	if (reports === null) {
-	        		reports = {};
-	        	}
-	        	res.status(200).send(reports);
-	        })
-	        .catch(err => {
-	        	res.status(412).send(err);
-	        });
-	} catch(err) {
-		res.status(412).send(err);
-	}
+    try {
+        const reportModel = sqldb().report;
+        reportModel.findOne({
+            where: {
+                    id: req.params.id
+                }
+            })
+            .then(reports => {
+                if (reports === null) {
+                    reports = {};
+                }
+                res.status(200).send(reports);
+            })
+            .catch(err => {
+                res.status(412).send(err);
+            });
+    } catch(err) {
+        res.status(412).send(err);
+    }
 });
 
 
 // initialize~
-http.createServer(app).listen(app.get('port'), function () {
-    console.log('Big Zam API listening on port %d...', app.get('port'));
+sqldb().sequelize.sync()
+.then(() => {
+    http.createServer(app).listen(app.get('port'), () => {
+        console.log('Big Zam API listening on port %d...', app.get('port'));
+    });
+})
+.catch(err => {
+    console.log(err);
+    process.exit();
 });
 
-process.on('SIGINT', function() {
+process.on('SIGINT', () => {
     process.exit();
 });
 
