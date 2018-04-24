92cea2ce69ee20da03900b859b50a2f9aa8f94d6
Richard Brantley <brantleyr@gmail.com>
2018-04-24 09:57:34 -0400

    add scan cancel logic

--- server.js
+++ server.js
@@ -8,11 +8,12 @@ const config = require('./config');
 const app = express();
 const env = process.env;
 const scanQueue = {};
+const scanStatus = {'scanning' : true, 'updated': new Date()};
 
 // app config
 app.set('port', config.port);
 
-// routes
+// Post a report
 app.post('/reports/:id', bodyParser.json({limit: '50mb'}), (req, res) => {
     console.log(`Incoming POST for image ID ${req.params.id}...`);
     const report = {
@@ -30,6 +31,7 @@ app.post('/reports/:id', bodyParser.json({limit: '50mb'}), (req, res) => {
     });
 });
 
+// Get all reports
 app.get('/reports', (req, res) => {
     const reportModel = sqldb().report;
     reportModel.findAll({
@@ -46,6 +48,7 @@ app.get('/reports', (req, res) => {
     });
 });
 
+// Get report by ID
 app.get('/reports/:id', (req, res) => {
     const reportModel = sqldb().report;
     reportModel.findOne({
@@ -64,7 +67,7 @@ app.get('/reports/:id', (req, res) => {
     });
 });
 
-// routes
+// Get entire queue
 app.get('/queue', (req, res) => {
     console.log(`Incoming GET for entire queue`);
     return res.status(200).send(scanQueue);
@@ -73,58 +76,24 @@ app.get('/queue', (req, res) => {
 // Queue an Image ID
 app.post('/queue/:id', bodyParser.json({limit: '50mb'}), (req, res) => {
     console.log(`Incoming POST Queue for image ID ${req.params.id}...`);
-    let concurrentScanLimit = process.env.CONCURRENT_SCAN_LIMIT ? parseInt(process.env.CONCURRENT_SCAN_LIMIT) : 2;
 
-    // If there are more than 2 scans going on then send back 403 (continue trying)
-    if ( scanQueue.length >= concurrentScanLimit ){
-        console.log(`Too many concurrent scans, sending timeout to scanner for ${req.params.id}...`);
-        return res.status(403).send();
-        
-    // If the Image already exists in the Queue then check when it was queued.
-    }else if ( req.params.id in scanQueue ){
-        let queueTime = new Date(scanQueue[req.params.id].queued);
-        let currTime = new Date();
-        let queueHoursElapsed = Math.abs(currTime - queueTime) / 36e5;
-        // If it was not queued within the past 24 hours allow
-        // This fixes edge cases where things were not dequeued properly
-        if ( queueHoursElapsed >= 24 ){
-            console.log(`Queue added for image ID ${req.params.id}...`);
-            scanQueue[req.params.id] = {
-                'image_id' : req.params.id,
-                'queued' : new Date()
-            };
-            return res.status(201).send();
-        }else{
-            console.log(`Queue exists for image ID ${req.params.id}...`);
-            return res.status(423).send();
-        }
+    // Check the current scan status
+    if ( scanStatus["scanning"] == true ){
+        let concurrentScanLimit = process.env.CONCURRENT_SCAN_LIMIT ? parseInt(process.env.CONCURRENT_SCAN_LIMIT) : 2;
 
-    // Otherwise check if it has been scanned within the past 24 hours
-    }else{
-
-        const reportModel = sqldb().report;
-        reportModel.findOne({
-            where: {
-                image_id: req.params.id
-            }
-        })
-        .then(reports => {
-            if (reports === null) {
-                reports = {};
-            }
-            if (!reports.hasOwnProperty('created_at')) {
-                console.log(`Queue added for image ID ${req.params.id}...`);
-                scanQueue[req.params.id] = {
-                    'image_id' : req.params.id,
-                    'queued' : new Date()
-                };
-                return res.status(201).send();
-            }
-
-            let reportTime = new Date(reports.created_at);
+        // If there are more than 2 scans going on then send back 403 (continue trying)
+        if ( scanQueue.length >= concurrentScanLimit ){
+            console.log(`Too many concurrent scans, sending timeout to scanner for ${req.params.id}...`);
+            return res.status(403).send();
+            
+        // If the Image already exists in the Queue then check when it was queued.
+        }else if ( req.params.id in scanQueue ){
+            let queueTime = new Date(scanQueue[req.params.id].queued);
             let currTime = new Date();
-            let reportHoursElapsed = Math.abs(currTime - reportTime) / 36e5;
-            if ( reportHoursElapsed < 24 ){
+            let queueHoursElapsed = Math.abs(currTime - queueTime) / 36e5;
+            // If it was not queued within the past 24 hours allow
+            // This fixes edge cases where things were not dequeued properly
+            if ( queueHoursElapsed >= 24 ){
                 console.log(`Queue added for image ID ${req.params.id}...`);
                 scanQueue[req.params.id] = {
                     'image_id' : req.params.id,
@@ -132,14 +101,65 @@ app.post('/queue/:id', bodyParser.json({limit: '50mb'}), (req, res) => {
                 };
                 return res.status(201).send();
             }else{
-                console.log(`Queue not added for image ID ${req.params.id}, scanned within past 24 hours`);
-                return res.status(412).send();
+                console.log(`Queue exists for image ID ${req.params.id}...`);
+                return res.status(423).send();
             }
-        })
-        .catch(err => {
-            return res.status(412).send(err);
-        });
 
+        // Otherwise check if it has been scanned within the past 24 hours
+        }else{
+
+            // Get the report from the database
+            const reportModel = sqldb().report;
+            reportModel.findOne({
+                where: {
+                    image_id: req.params.id
+                }
+            })
+            .then(reports => {
+                if (reports === null) {
+                    reports = {};
+                }
+                // Check if it actually has a created_at property (actually been scanned)
+                if (!reports.hasOwnProperty('created_at')) {
+                    console.log(`Queue added for image ID ${req.params.id}...`);
+                    scanQueue[req.params.id] = {
+                        'image_id' : req.params.id,
+                        'queued' : new Date()
+                    };
+                    return res.status(201).send();
+                }
+
+                // Calculate difference in hours (<24)
+                let reportTime = new Date(reports.created_at);
+                let currTime = new Date();
+                let reportHoursElapsed = Math.abs(currTime - reportTime) / 36e5;
+
+                // Only scan it if 24hrs have elapsed
+                if ( reportHoursElapsed < 24 ){
+                    console.log(`Queue added for image ID ${req.params.id}...`);
+                    scanQueue[req.params.id] = {
+                        'image_id' : req.params.id,
+                        'queued' : new Date()
+                    };
+                    return res.status(201).send();
+                }else{
+                    console.log(`Queue not added for image ID ${req.params.id}, scanned within past 24 hours`);
+                    return res.status(412).send();
+                }
+            })
+            .catch(err => {
+                return res.status(412).send(err);
+            });
+
+        }
+
+    // Otherwise scanning has halted
+    }else{
+        console.log('Scanning has been HALTED.');
+        console.log('Current queue and status:');
+        console.log(scanQueue);
+        console.log(scanStatus);
+        return res.status(409).send();
     }
 });
 
@@ -158,6 +178,32 @@ app.post('/dequeue/:id', bodyParser.json({limit: '50mb'}), (req, res) => {
         console.log(`No queue for image ID ${req.params.id}...`);
         return res.status(412).send();
     }
+});
+
+// Stop all image scans
+app.post('/halt', bodyParser.json({limit: '50mb'}), (req, res) => {
+    console.log('Incoming request to HALT all scanning.');
+    console.log('Any scans currently being performed will finish.');
+    console.log('No further scans will be allowed in the queue.');
+    console.log('The current queue is:');
+    console.log(scanQueue);
+    scanStatus["scanning"] = false;
+    scanStatus["updated"] = new Date();
+    return res.status(200).send();
+});
+
+// Resume all image scanning
+app.post('/resume', bodyParser.json({limit: '50mb'}), (req, res) => {
+    console.log('Incoming request to RESUME all scanning.');
+    scanStatus["scanning"] = true;
+    scanStatus["updated"] = new Date();
+    return res.status(200).send();
+});
+
+// Get current image scanning status
+app.get('/status', (req, res) => {
+    console.log(`Incoming GET for entire scanning status`);
+    return res.status(200).send(scanStatus);
 });
 
 
